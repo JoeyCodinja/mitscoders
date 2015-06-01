357876bb493ba36737560b4040586e58057d9a2d
Danuel Williams <joseph_thesaint@hotmail.com>
2015-06-01 16:43:26 -0500

    Inital Activities

---
+++ app/src/main/java/mits/uwi/com/ourmobileenvironment/BOSSActivity.java
@@ -0,0 +1,41 @@
+package mits.uwi.com.ourmobileenvironment;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class BOSSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_boss, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()){
+            //Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/mits/uwi/com/ourmobileenvironment/LandingActivity.java
@@ -0,0 +1,76 @@
+package mits.uwi.com.ourmobileenvironment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class LandingActivity extends AppCompatActivity {
+
+    private Button mToSASActivity, mToBOSSActivity, mToOURLVEActivity;
+
+    private static final String TAG = "LandingActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        mToSASActivity = (Button)findViewById(R.id.to_sas_activity_button);
+        mToSASActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LandingActivity.this, SASActivity.class);
+                Log.d(TAG, "Starting SASActivity");
+                startActivity(i);
+            }
+        });
+
+        mToBOSSActivity = (Button)findViewById(R.id.to_boss_activity_button);
+        mToBOSSActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LandingActivity.this, BOSSActivity.class);
+                Log.d(TAG, "Starting BOSSActivity");
+                startActivity(i);
+            }
+        });
+
+        mToOURLVEActivity = (Button)findViewById(R.id.to_ourvle_activity_button);
+        mToOURLVEActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LandingActivity.this, OurVLEActivity.class);
+                Log.d(TAG, "StartingOurVLEActivity");
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_landing, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
