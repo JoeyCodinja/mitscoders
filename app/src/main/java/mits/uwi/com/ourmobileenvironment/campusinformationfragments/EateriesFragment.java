ff9338d04a92312ba0b8c85be615dd6db7cde6d8
leonanto <leonardo.a.grant@gmail.com>
2015-07-22 07:29:10 -0500

    updated eateries component

--- app/src/main/java/mits/uwi/com/ourmobileenvironment/campusinformationfragments/EateriesFragment.java
+++ app/src/main/java/mits/uwi/com/ourmobileenvironment/campusinformationfragments/EateriesFragment.java
@@ -1,24 +1,58 @@
 package mits.uwi.com.ourmobileenvironment.campusinformationfragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mits.uwi.com.ourmobileenvironment.R;
 
 /**
  * Created by Danuel on 17/06/2015.
  */
-public class EateriesFragment extends Fragment {
+public class EateriesFragment extends AppCompatActivity {
+    private List<Eateries_list> eateries;
+    private RecyclerView rv;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_eateries, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_eateries);
+
+        rv=(RecyclerView)findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        initializeData();
+        initializeAdapter();
+    }
+
+
+    private void initializeData(){
+        eateries = new ArrayList<>();
+        eateries.add(new Eateries_list("KFC", "Ring Road, Chancellor Hall", "9am-12am", R.drawable.kfc));
+        eateries.add(new Eateries_list("Juici", "Faculty of Science and Technology","7am-7pm", R.drawable.juici));
+        eateries.add(new Eateries_list("Nardo's Snack Shop", "Humanities and Education", "6am-3am", R.drawable.nardo));
+    }
+
+    private void initializeAdapter(){
+        RVAdapter adapter = new RVAdapter(eateries);
+        rv.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/mits/uwi/com/ourmobileenvironment/campusinformationfragments/Eateries_list.java
@@ -0,0 +1,19 @@
+package mits.uwi.com.ourmobileenvironment.campusinformationfragments;
+
+/**
+ * Created by Leonardo on 7/6/2015.
+ */
+class Eateries_list {
+    String name;
+    String location;
+    String hours;
+    int photoId;
+
+    Eateries_list(String name, String location, String hours, int photoId) {
+        this.name = name;
+        this.location = location;
+        this.hours = hours;
+        this.photoId = photoId;
+    }
+}
+
