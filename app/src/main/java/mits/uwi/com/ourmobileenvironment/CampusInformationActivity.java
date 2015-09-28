03f19895eef0b991762941668cc2b84b35134a86
rox117 <therox116@gmail.com>
2015-09-28 14:14:54 -0500

    fixed merge conflict with eateries and directory

--- app/src/main/java/mits/uwi/com/ourmobileenvironment/CampusInformationActivity.java
+++ app/src/main/java/mits/uwi/com/ourmobileenvironment/CampusInformationActivity.java
@@ -17,10 +17,6 @@ public class CampusInformationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/demo
         setContentView(R.layout.activity_campus_information);
 
         FragmentManager fm = getSupportFragmentManager();
@@ -31,11 +27,6 @@ public class CampusInformationActivity extends AppCompatActivity {
             fm.beginTransaction()
                     .add(R.id.campusinfo_fragmentContainer, fragment)
                     .commit();
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/demo
         }
         //Calls to this function reposition the overflow
         ToprightBar.setTopOverflow(this);
