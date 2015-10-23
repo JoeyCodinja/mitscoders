df755058f7eae0b7d18a759c0e437044adaae712
Jevon Butler <jevon.butler@mymona.uwi.edu>
2015-10-22 20:39:37 -0500

    added Calendar for TimeTable

--- app/src/main/java/mits/uwi/com/ourmobileenvironment/sasfragments/timetablefragments/TeachingTimeTableFragment.java
+++ app/src/main/java/mits/uwi/com/ourmobileenvironment/sasfragments/timetablefragments/TeachingTimeTableFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import mits.uwi.com.ourmobileenvironment.R;
 
@@ -22,6 +24,12 @@ public class TeachingTimeTableFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_teaching_timetable,container,false);
+
+        Spinner spinner  = (Spinner) v.findViewById(R.id.semester);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.semester_test, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         return v;
     }
 }
