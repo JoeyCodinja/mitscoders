9115f85c96b4d897f87461410c8d36465a3a6b6e
Danuel Williams <joseph_thesaint@hotmail.com>
2015-10-30 10:31:46 -0430

    Refactored Code for Home page animations

--- app/src/main/java/mits/uwi/com/ourmobileenvironment/homepagefragments/HomeActivityFragment.java
+++ app/src/main/java/mits/uwi/com/ourmobileenvironment/homepagefragments/HomeActivityFragment.java
@@ -15,12 +15,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import mits.uwi.com.ourmobileenvironment.BOSSActivity;
 import mits.uwi.com.ourmobileenvironment.CampusInformationActivity;
 import mits.uwi.com.ourmobileenvironment.OurVLEActivity;
 import mits.uwi.com.ourmobileenvironment.R;
 import mits.uwi.com.ourmobileenvironment.SASActivity;
 import mits.uwi.com.ourmobileenvironment.adapters.HomePageViewPagerAdapter;
+
+import static java.lang.Math.pow;
 
 
 /**
@@ -30,8 +35,16 @@ public class HomeActivityFragment extends Fragment {
 
     private ImageView mFloatingActionButton, mFloatingActionButton2, mFloatingActionButton3,
             mFloatingActionButton4, mFloatingActionButton5, mFloatingActionButton6;
+    private ArrayList<ImageView> mFABs = new ArrayList<ImageView>();
     private Animator.AnimatorListener mFABAnimatorListener;
     public static Boolean mMenuExpanded = false;
+
+    int[] fabIDs = {R.id.landing_pageFAB,
+                    R.id.landing_pageFAB1,
+                    R.id.landing_pageFAB2,
+                    R.id.landing_pageFAB3,
+                    R.id.landing_pageFAB4,
+                    R.id.landing_pageFAB5};
 
     private ViewPager mHome_ViewPager;
     private Drawable[] mViewPagerTabIcons;
@@ -52,6 +65,7 @@ public class HomeActivityFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 
@@ -60,23 +74,26 @@ public class HomeActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_landing, container, false);
 
-        mFloatingActionButton = (ImageView)v.findViewById(R.id.landing_pageFAB);
-        mFloatingActionButton.bringToFront();
-        mFloatingActionButton2 = (ImageView)v.findViewById(R.id.landing_pageFAB1);
-        mFloatingActionButton2.setVisibility(View.INVISIBLE);
-        mFloatingActionButton3 = (ImageView)v.findViewById(R.id.landing_pageFAB2);
-        mFloatingActionButton3.setVisibility(View.INVISIBLE);
-        mFloatingActionButton4 = (ImageView)v.findViewById(R.id.landing_pageFAB3);
-        mFloatingActionButton4.setVisibility(View.INVISIBLE);
-        mFloatingActionButton5 = (ImageView)v.findViewById(R.id.landing_pageFAB4);
-        mFloatingActionButton5.setVisibility(View.INVISIBLE);
-        mFloatingActionButton6 = (ImageView)v.findViewById(R.id.landing_pageFAB5);
-        mFloatingActionButton6.setVisibility(View.INVISIBLE);
+        for(int fab = 0; fab <= fabIDs.length-1; fab ++){
+            mFABs.add((ImageView) v.findViewById(fabIDs[fab]));
+            if (fab > 0){
+                mFABs.get(fab).setVisibility(View.INVISIBLE);
+            }
+        }
+//        mFloatingActionButton = (ImageView)v.findViewById(R.id.landing_pageFAB);
+//        mFloatingActionButton2 = (ImageView)v.findViewById(R.id.landing_pageFAB1);
+//        mFloatingActionButton2.setVisibility(View.INVISIBLE);
+//        mFloatingActionButton3 = (ImageView)v.findViewById(R.id.landing_pageFAB2);
+//        mFloatingActionButton3.setVisibility(View.INVISIBLE);
+//        mFloatingActionButton4 = (ImageView)v.findViewById(R.id.landing_pageFAB3);
+//        mFloatingActionButton4.setVisibility(View.INVISIBLE);
+//        mFloatingActionButton5 = (ImageView)v.findViewById(R.id.landing_pageFAB4);
+//        mFloatingActionButton5.setVisibility(View.INVISIBLE);
+//        mFloatingActionButton6 = (ImageView)v.findViewById(R.id.landing_pageFAB5);
+//        mFloatingActionButton6.setVisibility(View.INVISIBLE);
 
         mFABAnimatorListener = new Animator.AnimatorListener() {
-            ImageView[] buttons = {mFloatingActionButton2,
-                    mFloatingActionButton3, mFloatingActionButton4,
-                    mFloatingActionButton5, mFloatingActionButton6};
+            List<ImageView> buttons = mFABs.subList(1,mFABs.size());
 
             @Override
             public void onAnimationStart(Animator animation) {
@@ -97,8 +114,9 @@ public class HomeActivityFragment extends Fragment {
 
             @Override
             public void onAnimationCancel(Animator animation) {
-                // TODO: Ensure that image views return to their base state
+                // TODO: Images return to base state; mMenu Expanded = False
                 // and the mMenuExpanded field is set to false
+                mMenuExpanded = false;
                 Log.d(TAG, "Animation Cancel");
 //                for (ImageView button: buttons){
 //                }
@@ -107,95 +125,118 @@ public class HomeActivityFragment extends Fragment {
             @Override
             public void onAnimationRepeat(Animator animation) {}
         };
-
-
-        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+        // Refactor
+        mFABs.get(0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!mMenuExpanded) {
-                    mFloatingActionButton2.setVisibility(View.VISIBLE);
-                    mFloatingActionButton2.animate()
-                            .translationXBy(-100f)
-                            .translationYBy(-150f)
-                            .setDuration(400)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton3.setVisibility(View.VISIBLE);
-                    mFloatingActionButton3.animate()
-                            .translationXBy(-200f)
-                            .translationYBy(-335f)
-                            .setDuration(400)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton4.setVisibility(View.VISIBLE);
-                    mFloatingActionButton4.animate()
-                            .translationXBy(-245f)
-                            .translationYBy(-545f)
-                            .setDuration(400)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton5.setVisibility(View.VISIBLE);
-                    mFloatingActionButton5.animate()
-                            .translationXBy(-200f)
-                            .translationYBy(-750f)
-                            .setDuration(400)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton6.setVisibility(View.VISIBLE);
-                    mFloatingActionButton6.animate()
-                            .translationXBy(-100f)
-                            .translationYBy(-950f)
-                            .setDuration(400)
-                            .setListener(mFABAnimatorListener);
-
+                if (!mMenuExpanded){
+                    for (int fab = 1; fab <= mFABs.size()-1; fab++){
+                        mFABs.get(fab).setVisibility(View.VISIBLE);
+                        mFABs.get(fab).animate()
+                                .setDuration(400)
+                                .translationXBy((float) -((-35 * pow(fab, 2)) + (210 * fab) - 75))
+                                .translationYBy((float) -((150.83 * pow(fab, 1.1533f))))
+                                .setListener(mFABAnimatorListener);
+                    }
                     mMenuExpanded = true;
                 } else {
-
-
-                    mFloatingActionButton2.animate()
-                            .translationXBy(100f)
-                            .translationYBy(150f)
-                            .setDuration(400)
-                            .alpha(0)
-                            .setDuration(3500)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton3.animate()
-                            .translationXBy(200f)
-                            .translationYBy(335f)
-                            .setDuration(400)
-                            .alpha(0)
-                            .setDuration(3000)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton4.animate()
-                            .translationXBy(245f)
-                            .translationYBy(545f)
-                            .setDuration(400)
-                            .alpha(0)
-                            .setDuration(2500)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton5.animate()
-                            .translationXBy(200f)
-                            .translationYBy(750f)
-                            .setDuration(400)
-                            .alpha(0)
-                            .setDuration(2000)
-                            .setListener(mFABAnimatorListener);
-
-                    mFloatingActionButton6.animate()
-                            .translationXBy(100f)
-                            .translationYBy(950f)
-                            .setDuration(400)
-                            .alpha(0)
-                            .setDuration(1500)
-                            .setListener(mFABAnimatorListener);
-
+                    for (int fab = 1; fab <= mFABs.size()-1; fab ++){
+                        mFABs.get(fab).animate()
+                                .setDuration(3500 - (500 * (fab - 1)))
+                                .translationXBy((float) (-35 * (pow(fab, 2))) + (210 * fab) - 75)
+                                .translationYBy((float) (150.83 * (pow(fab, 1.1533f))))
+                                .alpha(0)
+                                .setListener(mFABAnimatorListener);
+                    }
                     mMenuExpanded = false;
                 }
             }
         });
+
+
+        //original
+//        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (!mMenuExpanded) {
+//                    mFloatingActionButton2.setVisibility(View.VISIBLE);
+//                    mFloatingActionButton2.animate()
+//                            .translationXBy(-100f)
+//                            .translationYBy(-150f)
+//                            .setDuration(400)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton3.setVisibility(View.VISIBLE);
+//                    mFloatingActionButton3.animate()
+//                            .translationXBy(-200f)
+//                            .translationYBy(-335f)
+//                            .setDuration(400)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton4.setVisibility(View.VISIBLE);
+//                    mFloatingActionButton4.animate()
+//                            .translationXBy(-245f)
+//                            .translationYBy(-545f)
+//                            .setDuration(400)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton5.setVisibility(View.VISIBLE);
+//                    mFloatingActionButton5.animate()
+//                            .translationXBy(-200f)
+//                            .translationYBy(-750f)
+//                            .setDuration(400)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton6.setVisibility(View.VISIBLE);
+//                    mFloatingActionButton6.animate()
+//                            .translationXBy(-100f)
+//                            .translationYBy(-950f)
+//                            .setDuration(400)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mMenuExpanded = true;
+//                } else {
+//
+//
+//                    mFloatingActionButton2.animate()
+//                            .translationXBy(100f)
+//                            .translationYBy(150f)
+//                            .alpha(0)
+//                            .setDuration(3500)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton3.animate()
+//                            .translationXBy(200f)
+//                            .translationYBy(335f)
+//                            .alpha(0)
+//                            .setDuration(3000)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton4.animate()
+//                            .translationXBy(245f)
+//                            .translationYBy(545f)
+//                            .alpha(0)
+//                            .setDuration(2500)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton5.animate()
+//                            .translationXBy(200f)
+//                            .translationYBy(750f)
+//                            .alpha(0)
+//                            .setDuration(2000)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mFloatingActionButton6.animate()
+//                            .translationXBy(100f)
+//                            .translationYBy(950f)
+//                            .alpha(0)
+//                            .setDuration(1500)
+//                            .setListener(mFABAnimatorListener);
+//
+//                    mMenuExpanded = false;
+//                }
+//            }
+//        });
 
         View.OnClickListener FABClickListener = new View.OnClickListener() {
             Intent i;
